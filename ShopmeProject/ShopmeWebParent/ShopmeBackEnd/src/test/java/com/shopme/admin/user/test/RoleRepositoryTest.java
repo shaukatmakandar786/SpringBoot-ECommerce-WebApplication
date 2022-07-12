2ad6b3b928a6b25e73bbd217ec0d009b04005ab1
Shaukat Makandar <shaukatmakandar786@gmail.com>
2022-07-12 22:48:40 +0530

    added files

---
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/test/RoleRepositoryTest.java
@@ -0,0 +1,18 @@
+package com.shopme.admin.user.test;
+
+
+//@DataJpaTest
+//@AutoConfigureTestDatabase(replace = Replace.NONE)
+
+public class RoleRepositoryTest {
+
+//	@Autowired
+//	private RoleRepository roleRepository;
+//	
+//	@Test
+//	public void testCreateFirstRole(){
+//		
+//	
+//	}
+	
+}
