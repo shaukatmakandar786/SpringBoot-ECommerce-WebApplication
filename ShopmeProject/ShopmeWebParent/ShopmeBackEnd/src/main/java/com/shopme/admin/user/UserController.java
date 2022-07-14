c5c2f6498fbcd044828b1d1d2f92301025b598d1
Shaukat Makandar <shaukatmakandar786@gmail.com>
2022-07-14 18:56:58 +0530

    added files

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.shopme.common.entity.User;
 
@@ -18,9 +19,25 @@ public class UserController {
 	@GetMapping("/users")
 	public String listAll(Model model)
 	{
+		
 		List<User> userList = userService.listAll();
 		model.addAttribute("userList",userList);
 		
 		return "users";
+	}
+	
+	@GetMapping("/users/new")
+	public String newUser(Model model) {
+		
+		User user=new User();
+		model.addAttribute("user", user);
+		return "user_form";
+	}
+	
+	@GetMapping("/users/save")
+	public String saveUser(User user) {
+		
+		System.out.println(user);
+		return "redirect:/users";
 	}
 }
